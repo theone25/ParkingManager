044159599311d3886d455290e4d406d4f753e9f8
Mohamed Bouziani <mohamedbouziani25@gmail.com>
2021-03-25 12:52:11 +0100

    added BCrypt and WebcamCapture packages

--- src/main/java/com/parkingmanager/controllers/dashboardController.java
+++ src/main/java/com/parkingmanager/controllers/dashboardController.java
@@ -1,5 +1,6 @@
 package com.parkingmanager.controllers;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import javafx.scene.control.Button;
 
 public class dashboardController {
@@ -10,4 +11,13 @@ public class dashboardController {
     public Button button4;
     public Button button5;
     public Button button6;
+
+    public void testfunction(){
+        String password = "1234";
+        String bcryptHashString = BCrypt.withDefaults().hashToString(12, password.toCharArray());
+        // $2a$12$US00g/uMhoSBm.HiuieBjeMtoN69SN.GE25fCpldebzkryUyopws6
+
+        BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), bcryptHashString);
+        // result.verified == true
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.parkingmanager {
     //requires org.jfxtras.styles.jmetro;
     requires activejdbc;
     requires java.sql;
+    requires bcrypt;
 
     opens com.parkingmanager.controllers to javafx.fxml;
     exports com.parkingmanager.controllers;
