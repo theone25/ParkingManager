98677c249d2894d4f90d34e071b6768ac9f63c39
Mohamed Bouziani <mohamedbouziani25@gmail.com>
2021-03-25 15:34:49 +0100

    added navigation between multiple views

--- src/main/java/com/parkingmanager/controllers/dashboardController.java
+++ src/main/java/com/parkingmanager/controllers/dashboardController.java
@@ -1,7 +1,17 @@
 package com.parkingmanager.controllers;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.parkingmanager.App;
+import com.parkingmanager.dao.HelperDB;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class dashboardController {
 
@@ -11,6 +21,22 @@ public class dashboardController {
     public Button button4;
     public Button button5;
     public Button button6;
+
+    @FXML Pane mainPane;
+
+    public void GetCameraStream(){
+        try {
+            Pane cameraStreamPane =  FXMLLoader.load(App.class.getResource("views/cameraStream.fxml"));
+
+            mainPane.getChildren().add(cameraStreamPane);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
 
     public void testfunction(){
         String password = "1234";
