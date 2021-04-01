e11f95935b4b5067d5f290912dc0204cacf9ef12
Mohamed Bouziani <mohamedbouziani25@gmail.com>
2021-04-01 16:51:22 +0100

    added info display while hovering over a place

--- src/main/java/com/parkingmanager/controllers/PlacesPageController.java
+++ src/main/java/com/parkingmanager/controllers/PlacesPageController.java
@@ -4,19 +4,25 @@ import com.parkingmanager.App;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
+import javafx.stage.PopupWindow;
+import javafx.util.Duration;
 import org.w3c.dom.events.MouseEvent;
 
 public class PlacesPageController {
     @FXML
     TilePane placesPane;
+    private static final String SQUARE_BUBBLE =
+            "M24 1h-24v16.981h4v5.019l7-5.019h13z";
 
-    @FXML
-    public void hoverPlace(AnchorPane place ,MouseEvent event)
-    {
-        place.setStyle("-fx-background-color: #121212");
+    private Tooltip makeBubble(Tooltip tooltip) {
+        tooltip.setStyle("-fx-font-size: 16px; -fx-shape: \"" + SQUARE_BUBBLE + "\";");
+        tooltip.setAnchorLocation(PopupWindow.AnchorLocation.WINDOW_BOTTOM_LEFT);
+        tooltip.setShowDelay(Duration.millis(100));
+        return tooltip;
     }
 
     @FXML
@@ -35,6 +41,7 @@ public class PlacesPageController {
                     place.setStyle("-fx-background-color: green");
                 });
                 Label lb= (Label) place.getChildren().get(0);
+                Tooltip.install(place,makeBubble(new Tooltip(String.valueOf("ggggggggggggggggggggggggggggggggggggggggggggg"))));
                 lb.setText(String.valueOf(i));
                 placesPane.getChildren().add(place);
             }catch (Exception e){
